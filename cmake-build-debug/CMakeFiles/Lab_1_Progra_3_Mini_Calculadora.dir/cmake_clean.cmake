6dd387cc12238dea9ea5deeace7abe65afa155c8
Oscarluiis <tinocoscar11@unitec.edu>
2017-07-25 10:18:22 -0600

    Ejercicio Mini Calculadora C++.
    Ejercicio de Laboratorio.

---
+++ cmake-build-debug/CMakeFiles/Lab_1_Progra_3_Mini_Calculadora.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_1_Progra_3_Mini_Calculadora.dir/main.cpp.o"
+  "Lab_1_Progra_3_Mini_Calculadora.pdb"
+  "Lab_1_Progra_3_Mini_Calculadora"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_1_Progra_3_Mini_Calculadora.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main() {
+
+    //variables
+    double num_1=0, num_2=0, resul=0;
+    char op;
+
+    //Pedir ingreso
+    cout<<"Ingrese primer numero :";
+    cin>>num_1;
+
+    cout<<"Ingrese operador :";
+    cin>>op;
+
+    cout<<"Ingrese segundo numero";
+    cin>>num_2;
+
+    //Condiciones
+    if (op == '+'){
+        resul = num_1 + num_2;
+        cout<<"La suma de los dos numero es :"<<resul;
+    } else if(op == '-'){
+        resul = num_1 - num_2;
+        cout<<"La resta de los dos numeros es :"<<resul;
+    }else if(op == '*'){
+        resul = num_1 * num_2;
+        cout<<"La multiplicacion de los dos numero es :"<<resul;
+    }else if(op == '/'){
+        resul = num_1 / num_2;
+        cout<<"La division de los dos numero es :"<<resul;
+    }else{
+        cout<<"Intente con un operador valido";
+    }
+
+    return 0;
+
+}
+
